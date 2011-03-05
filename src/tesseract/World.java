78ddd0494aa7b9adba29630ffd28454da9e92d3a
Steve <steveb9@u.washington.edu>
2011-03-05 13:12:03 +0000

    getting late need break

--- src/tesseract/World.java
+++ src/tesseract/World.java
@@ -270,24 +270,28 @@ public class World implements Observer {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side1.getPosition().getX(), my_side1.getPosition().getY()), myObjects.get(j));
 						myObjects.get(j).detach();
+						myObjects.remove(j);
 					}
 					if (myObjects.get(i).equals(my_side2)&& myPeer.getPeerInDirection
 							(my_side2.getPosition().getX(), my_side2.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side2.getPosition().getX(), my_side2.getPosition().getY()), myObjects.get(j));
 						myObjects.get(j).detach();
+						myObjects.remove(j);
 					} 
 					if (myObjects.get(i).equals(my_side3)&& myPeer.getPeerInDirection
 							(my_side3.getPosition().getX(), my_side3.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side3.getPosition().getX(), my_side3.getPosition().getY()), myObjects.get(j));
 						myObjects.get(j).detach();
+						myObjects.remove(j);
 					} 
 					if (myObjects.get(i).equals(my_side4)&& myPeer.getPeerInDirection
 							(my_side4.getPosition().getX(), my_side4.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side4.getPosition().getX(), my_side4.getPosition().getY()), myObjects.get(j));
 						myObjects.get(j).detach();
+						myObjects.remove(j);
 					}
 				
 				//if 'j' is a side transmit i object
@@ -296,24 +300,28 @@ public class World implements Observer {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side1.getPosition().getX(), my_side1.getPosition().getY()), myObjects.get(i));
 						myObjects.get(i).detach();
+						myObjects.remove(i);
 					} 
 					if (myObjects.get(j).equals(my_side2)&& myPeer.getPeerInDirection
 							(my_side2.getPosition().getX(), my_side2.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side2.getPosition().getX(), my_side2.getPosition().getY()), myObjects.get(i));
 						myObjects.get(i).detach();
+						myObjects.remove(i);
 					} 
 					if (myObjects.get(j).equals(my_side3)&& myPeer.getPeerInDirection
 							(my_side3.getPosition().getX(), my_side3.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side3.getPosition().getX(), my_side3.getPosition().getY()), myObjects.get(i));
 						myObjects.get(i).detach();
+						myObjects.remove(i);
 					} 
 					if (myObjects.get(j).equals(my_side4)&& myPeer.getPeerInDirection
 							(my_side4.getPosition().getX(), my_side4.getPosition().getY()) != null) {
 						myPeer.sendPayloadToPeer(myPeer.getPeerInDirection
 								(my_side4.getPosition().getX(), my_side4.getPosition().getY()), myObjects.get(i));
 						myObjects.get(i).detach();
+						myObjects.remove(i);
 					}
 					myObjects.get(i).resolveCollisions(myObjects.get(j));
 				}
