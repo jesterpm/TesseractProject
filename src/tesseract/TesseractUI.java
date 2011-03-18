1c7d9d9144f906204001afd1c76fd47ef0548b88
Jesse Morgan <jesse@jesterpm.net>
2011-03-18 05:49:02 +0000

    Cleanup

--- src/tesseract/TesseractUI.java
+++ src/tesseract/TesseractUI.java
@@ -168,9 +168,6 @@ public class TesseractUI extends JFrame {
 		createBackground();
 		createGround();
 		
-		//Blimp blimp = new Blimp(new Vector3f(0,0,0), .7f);
-		//myWorld.addObject(blimp);
-		
 		myCurrentObject = null;
 		
 		myObjectMenuItems = new JMenuItem[] {
